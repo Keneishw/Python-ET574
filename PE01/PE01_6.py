9ab65c94ac74bb3a5ea55ee1e69e22676cd267b6
Keneishw <keneish.wright07@gmail.com>
2025-02-04 21:00:10 +0000

    verfe

---
+++ PE01/PE01_6.py
@@ -0,0 +1,29 @@
+print(2+3*4)
+print(1-7**2) 
+print(1//2**3)
+#print(3+4)*5 
+print(5 % 3 )* 4 
+print(-2)**(-2)
+print(7//3) 
+print(14%4)
+print(1+7%4)
+print(14//4*4)
+print(5//2+2) 
+print(5%5*5)
+13 – 18 determine whether the name is a valid variable name. Explain your answer.
+13) NewYear.sales 
+14) room&color 
+15) TOrF_1040
+16) 311HotLine 
+17) expense# 
+18) INCOME 101
+19 – 
+24 rewrite the statements using augmented assignment operators.
+19) cost = cost + 5 
+20) sum = sum * rate 
+21) product = product / 10
+22) cost = cost // num 
+23) total = total – cost 
+24) quotient = quotient % rate
+25 – 27 find the value of the function where a = 5 and b = 3.
+25) int(-a / b) 26) round(a / b, 2) 27) abs(b - a)
