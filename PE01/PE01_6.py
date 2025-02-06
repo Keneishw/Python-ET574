500690dd39207b4f30301b8c2f1aa155788f985d
Keneishw <keneish.wright07@gmail.com>
2025-02-06 02:47:52 +0000

    tfuk

--- PE01/PE01_6.py
+++ PE01/PE01_6.py
@@ -27,3 +27,44 @@ print(5%5*5)
 24) quotient = quotient % rate
 25 – 27 find the value of the function where a = 5 and b = 3.
 25) int(-a / b) 26) round(a / b, 2) 27) abs(b - a)
+
+
+print(2+3*4)
+print(1-7**2)
+print(1//2**3)
+print(3+4)*5
+print(5%3)*4
+print(-2)**(-2)
+print(7//3)
+print(14%4)
+print(1+7%4)
+print(14//4*4)
+print(5//2+2)
+print(5%5*5)
+
+NewYear.sales #invalid the period is a sepecial character.
+ room&color #invalid & is a special character
+ TOrF_1040 # invalid _ is a special character
+ 311HotLine # invalid this starts with a number
+ expense#  # invalid # is a special character
+ INCOME 101 # valiid
+
+ cost= 8
+ sum= 5
+ rate= 7
+ total=9
+ product=20
+ quotient= 16
+
+print(cost+5=)
+print(sum*rate=)
+print(product/10=)
+print(cost//num)
+print(total-cost=)
+print(quotient%rate=)
+
+a=5
+b=3
+int(-a/b)
+round(a/b,2)
+abs(b-a)
